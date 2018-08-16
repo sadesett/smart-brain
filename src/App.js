825ee402c766c3d255d42636a3d815516ab5aa98
sadesett <sadeset@gmail.com>
2018-08-16 18:38:53 +0300

    smart-brain-app

---
+++ src/App.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import './App.css';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Register from './components/Register/Register';
+import Logo from './components/Logo/Logo';
+import SignIn from './components/SignIn/SignIn';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Facerecognition from './components/Facerecognition/Facerecognition';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedin: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  loadUser = data => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    });
+  };
+
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = box => {
+    this.setState({ box: box });
+  };
+
+  onInputChange = event => {
+    this.setState({ input: event.target.value });
+  };
+
+  onPictureSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    fetch('https://radiant-sands-60941.herokuapp.com/imageUrl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
+      .then(response => {
+        if (response) {
+          fetch('https://radiant-sands-60941.herokuapp.com/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            })
+            .catch(console.log);
+        }
+
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      .catch(err => console.log(err, 'error'));
+  };
+
+  onRouteChange = route => {
+    if (route === 'signout') {
+      this.setState(initialState);
+    } else if (route === 'home') {
+      this.setState({ isSignedin: true });
+    }
+    this.setState({ route: route });
+  };
+
+  render() {
+    const { isSignedin, imageUrl, route, box } = this.state;
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation
+          isSignedin={isSignedin}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === 'home' ? (
+          <div>
+            <Logo />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onPictureSubmit={this.onPictureSubmit}
+            />
+            <Facerecognition box={box} imageUrl={imageUrl} />
+          </div>
+        ) : route === 'signin' ? (
+          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Facerecognition/Facerecognition.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Facerecognition.css';
+
+const Facerecognition = ({ imageUrl, box }) => {
+  return (
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img
+          id="inputImage"
+          alt=""
+          src={imageUrl}
+          width="500px"
+          height="auto"
+        />
+        <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}} />
+      </div>
+    </div>
+  );
+};
+
+export default Facerecognition;
